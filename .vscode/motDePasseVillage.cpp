a881c1cbde40e0c593755bb21aaf7cedf2697e5d
naouris <nawresoueslati2@gmail.com>
2022-06-03 14:32:32 +0200

    tester et comparer code correspond au code secret

---
+++ .vscode/motDePasseVillage.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int code;
+    cin>>code;
+
+    if (code ==64741)
+    cout<< "Bon festin !" <<endl;
+    else
+    cout<<"Allez-vous en !"<<endl;
+    
+	return 0;
+}
